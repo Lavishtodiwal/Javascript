1d3f070575a0a18d11b2f42eee564f49b38e4a89
Lavish Todiwal <lavishtodiwal7455@gmail.com>
2026-04-03 23:47:30 +0530

    dom finish

---
+++ DOM/dom.js
@@ -0,0 +1,10 @@
+// title.textContent -> returns also the hidden html tags which are display:none
+// 'Dom Learning '
+// title.innerHTML -> gives the html content
+// 'Dom Learning '
+// title.innerText -> returns the text
+// 'Dom Learning'
+
+
+
+// html collections -> Array.from("collection name")
